c5cd4d0d920c40d2ed3436da32580223e9429bef
Aakash Narkar <56544770+AakashNarkar@users.noreply.github.com>
2023-02-07 16:27:11 +0530

    script

---
+++ sample-build-scripts/ios/appcenter-beta-sdk/appcenter-pre-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# Download the latest state (`develop` branch) of the iOS SDK https://github.com/Microsoft/AppCenter-SDK-Apple
+# And overwrite the files in the respective location of the Xcode project.
+
+if 1 == 1;
+then
+    echo "Hello World!"
+fi
